5da36331d96d7313adfa258c643fc0917cc1f295
Ander <armath123@gmail.com>
2019-05-07 20:38:22 +1000

    Update executable module

--- test.cpp
+++ test.cpp
@@ -9,6 +9,8 @@ int main() {
 	std::string str; std::getline(std::cin, str);
 	TREE.create(str);
 	std::cout << TREE << std::endl;
+	TREE.reduce();
+	std::cout << TREE << std::endl;
 	TREE.derive('x');
 	std::cout << TREE << std::endl;
 	TREE.reduce();
